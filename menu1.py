a6faddd546749c013b730a582e302ef5b0d84945
sams2211 <70215063+sams2211@users.noreply.github.com>
2020-09-29 21:12:33 +0530

    Add files via upload

---
+++ menu1.py
@@ -0,0 +1,28 @@
+import webbrowser
+import speech_recognition as sr
+import pyttsx3
+
+r = sr.Recognizer()
+
+print("welcome to my tools")
+pyttsx3.speak("welcome to my tools")
+
+
+print("enter ur requirements:")
+
+with sr.Microphone() as source:
+    print('start say ....')
+    audio = r.record(source,duration=4)
+    print('speech done')
+
+ch=r.recognize_google(audio)
+print(ch)
+if "date" in ch:
+	webbrowser.open("http://192.168.0.151/cgi-bin/1.py?c=date")
+elif "calendar" in ch:
+	webbrowser.open("http://192.168.0.151/cgi-bin/1.py?c=cal")
+elif ("search" in ch) and ("Google" in ch):
+	webbrowser.open("https://google.com/?q=%s"%(ch))
+else:
+	print("not understand")
+
